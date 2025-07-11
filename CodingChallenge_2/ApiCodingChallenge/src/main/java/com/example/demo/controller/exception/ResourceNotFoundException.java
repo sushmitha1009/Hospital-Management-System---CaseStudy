9067400e188fa5b88572e2f617db4a2f28532775
SUSHMITHA S <122036827+sushmitha1009@users.noreply.github.com>
2025-07-11 14:02:50 +0530

    Delete CodingChallenge_2/ApiCodingChallenge directory

--- CodingChallenge_2/ApiCodingChallenge/src/main/java/com/example/demo/controller/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-	public ResourceNotFoundException(String error) {
-		super(error);
-	}
-}
-
